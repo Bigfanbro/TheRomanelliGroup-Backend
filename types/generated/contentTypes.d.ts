75ffe1ab8ce1bebc6e9350a7ff9d54b16947bf1c
Mohd Sameer <83845532+sameer3134@users.noreply.github.com>
2025-11-19 23:20:17 +0530

    families we served2

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -451,7 +451,7 @@ export interface ApiFamiliesWeServedFamiliesWeServed
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    Image_url: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    Image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
