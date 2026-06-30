037063ba3e0b955f1dc4b24f8705a56fc93daa68
Big Fan Bro <rajatsharma270@gmail.com>
2026-06-30 20:40:58 +0530

    API contact controller

---
+++ src/api/contact/controllers/contact.ts
@@ -0,0 +1,10 @@
+import { Context } from "koa";
+
+export default {
+  async submit(ctx: Context) {
+    ctx.body = {
+      success: true,
+      message: "Contact endpoint is working!",
+    };
+  },
+};
